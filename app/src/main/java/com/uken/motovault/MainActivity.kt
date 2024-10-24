a4dd0330eb0bebb6794f4a19d18d88857059bdd1
magmatt <matiwojt@gmail.com>
2024-10-24 19:01:57 +0200

    test commit

--- app/src/main/java/com/uken/motovault/MainActivity.kt
+++ app/src/main/java/com/uken/motovault/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MotoVaultTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Hello there I am working",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
