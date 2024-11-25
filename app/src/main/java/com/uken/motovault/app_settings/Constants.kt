5b32e58413cbc8577552122124327116de9fe0f3
Mateusz Wójtowicz <matiwojt@gmail.com>
2024-11-25 02:33:40 +0100

    initial SettingsScreen design, add Constants object

---
+++ app/src/main/java/com/uken/motovault/app_settings/Constants.kt
@@ -0,0 +1,5 @@
+package com.uken.motovault.app_settings
+
+object Constants {
+    const val SUPPORT_EMAIL = "support@moto-vault.com"
+}
--- app/src/main/java/com/uken/motovault/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/uken/motovault/ui/screens/SettingsScreen.kt
@@ -1,23 +1,44 @@
 package com.uken.motovault.ui.screens
 
+import android.content.Intent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForwardIos
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.uken.motovault.app_settings.Constants
 import com.uken.motovault.sign_in.email_sign_in.EmailSignInViewModel
 import com.uken.motovault.sign_in.google_sign_in.UserData
 import com.uken.motovault.ui.composables.app_navigation_drawer.AppNavigationDrawer
 import com.uken.motovault.ui.composables.navigationbar.AppNavigationBar
 import com.uken.motovault.ui.composables.navigationbar.NavigationBarViewModel
+import com.uken.motovault.ui.composables.settings_screen.SettingsSectionTitle
 import com.uken.motovault.ui.composables.top_app_bar.TopAppBar
 
 @Composable
@@ -30,6 +51,10 @@ fun SettingsScreen(
 ) {
     val scope = rememberCoroutineScope()
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val context = LocalContext.current
+
+    var notificationsEnabled by remember { mutableStateOf(true) }
+    var oilChangeInterval by remember { mutableStateOf("Every year") }
 
     ModalNavigationDrawer(
         drawerState = drawerState,
@@ -52,7 +77,81 @@ fun SettingsScreen(
                     .padding(paddingValues),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
+                Spacer(modifier = Modifier.padding(top = 8.dp))
+                SettingsSectionTitle("Notifications")
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "Enable notifications")
+                    Switch(
+                        checked = notificationsEnabled,
+                        onCheckedChange = { notificationsEnabled = it }
+                    )
+                }
 
+                HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "Oil change interval")
+                    TextButton(
+                        onClick = { /* Handle interval change */ }
+                    ) {
+                        Text(oilChangeInterval)
+                    }
+                }
+
+                HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
+
+                SettingsSectionTitle("App")
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp, vertical = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    TextButton(onClick = {  }) {
+                        Text("About app")
+                        Icon(Icons.Filled.ArrowForwardIos, contentDescription = "ArrowForwardIos")
+                    }
+                }
+
+                HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
+
+                SettingsSectionTitle("Issues", color = Color.Red)
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp, vertical = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    TextButton(
+                        onClick = {
+                            val emailIntent = Intent(Intent.ACTION_SEND).apply {
+                                type = "plain/text"
+                                putExtra(Intent.EXTRA_EMAIL, arrayOf(Constants.SUPPORT_EMAIL))
+                                putExtra(Intent.EXTRA_SUBJECT, "Bug Report")
+                                putExtra(Intent.EXTRA_TEXT, "Bug description: ")
+                            }
+                            context.startActivity(emailIntent) }
+                    ) {
+                        Text("Report bug")
+                        Icon(Icons.Filled.ArrowForwardIos, contentDescription = "ArrowForwardIos")
+                    }
+                }
             }
         }
     }
