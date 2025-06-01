1e0710561df76ca2a0ea3f030c06fbe26a616de8
Mateusz Wójtowicz <matiwojt@gmail.com>
2025-06-01 15:09:10 +0200

    change Constants.kt to blank strings

--- mobile/app/src/main/java/com/uken/motovault/api/Constants.kt
+++ mobile/app/src/main/java/com/uken/motovault/api/Constants.kt
@@ -3,11 +3,14 @@ package com.uken.motovault.api
 object Constants {
 
     // Spring Boot REST API config
-    const val REST_API_BASE_URL = "http://147.185.221.21:35978/"
+    const val REST_API_BASE_URL = ""
 
     // VIN Decoder API Config
-    const val VIN_DECODER_BASE_URL = "https://api.vindecoder.eu/3.2"
-    const val VIN_DECODER_API_KEY = "2e3d0dea0d46"
-    const val VIN_DECODER_SECRET_KEY = "950474af86"
-    const val VIN_DECODER_ACTION = "decode"
+    const val VIN_DECODER_BASE_URL = ""
+    const val VIN_DECODER_API_KEY = ""
+    const val VIN_DECODER_SECRET_KEY = ""
+    const val VIN_DECODER_ACTION = ""
+
+    // Microservice connection
+    const val MICROSERVICE_CONNECTION_STRING = ""
 }
